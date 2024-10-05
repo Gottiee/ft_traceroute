ee0bfaf0e294a6b32e17e84deaf519d2d1bbe76f
gottiee <gottiedev@gmail.com>
2024-10-05 09:44:12 -0400

    first push

---
+++ inc/traceroute.h
@@ -0,0 +1,18 @@
+#ifndef TRACEROUTE
+#define TRACEROUTE
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/ip_icmp.h>
+#include <time.h>
+
+
+#endif
---
+++ obj/traceroute.d
@@ -0,0 +1,2 @@
+obj/traceroute.o: src/traceroute.c src/../inc/traceroute.h
+src/../inc/traceroute.h:
